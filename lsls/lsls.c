45d415ded73e28160c43bd7e70fa1aa4ee7e5847
Wanda <doubleuevans@gmail.com>
2019-05-03 11:01:47 -0500

    attempting to assign argv, not returning anything.

--- lsls/lsls.c
+++ lsls/lsls.c
@@ -1,30 +1,42 @@
 #include <stdio.h>
 #include <dirent.h>
+#include <sys/stat.h>
+#include <stdlib.h>
+#include <unistd.h>
 
 /**
  * Main
  */
-int main(int argc, char **argv)
+int main(int argc, char **argv) 
+//argv array of char pointers aka strings
+//argc is the count of cmdline arguments
 {
   // Parse command line
+
+  // printf("There are %d command line argument(s):\n", argc);
+  char *file;
+
   int i;
+  if (argc == 1) //if there is only one argument dr = "."
+  {
+    file = ".";
+    return 0;
+  }
 
-  printf("There are %d command line argument(s):\n", argc);
-
-  for (i = 0; i < argc; i++) {
+  for (i = 1; i < argc; i++) {
       printf("   %s\n", argv[i]);
   }
 
   // Open directory
+  DIR *dr = opendir(file); //opendir returns a pointer of DIR type
+  //open dir opens path in (), the period means current directory
   struct dirent *entry; //pointer for directory entry
 
-  DIR *dr = opendir("."); //opendir returns a pointer of DIR type
-  //open dir opens path in (), the period means current directory
 
   if (dr == NULL) //return null if cant open
   {
     printf("dir open failed");
-    return 0;
+    exit(0); // closes the program
   }
   // Repeatly read and print entries
   while((entry = readdir(dr)) != NULL)
